2befeb6700b1638606489da157275f3432e629b2
Rishi <rishikm215@gmail.com>
2025-10-24 22:15:52 +0530

    added main vite files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { DraggableCardDemo } from "./components/DragCardPreview";
+import Home from "./components/Home";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <DraggableCardDemo />
+          
+    </>
+  );
+}
