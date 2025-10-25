2eb093eed528e328c7a9acc887e1fd43e6fde439
Rishi <rishikm215@gmail.com>
2025-10-25 15:06:50 +0530

    added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-import { DraggableCardDemo } from "./components/DragCardPreview";
+
 import Home from "./components/Home";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <>
       <Home />
-      <DraggableCardDemo />
+      <h1 className="bg-slate-900 font-bold text-white text-center text-4xl z-10 overflow-x-hidden " >Skills</h1>
+      <div className="flex justify-center bg-slate-900  border-amber-100" >
+        <Skills />
+      </div>
           
     </>
   );
