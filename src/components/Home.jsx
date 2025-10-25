b5baab7af9cd2cce17d3748fc5c387663f2c1ad6
Rishi <rishikm215@gmail.com>
2025-10-25 15:00:11 +0530

    removed overflow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { SpotlightPreview } from "./SpotlightPreview";
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+      <div className="h-screen overflow-x-hidden  w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
         <Boxes />
         <SpotlightPreview />
       </div>
