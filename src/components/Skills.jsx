c07ba0f56f245efc42441c88bff7a4e03bf96c25
Rishi <rishikm215@gmail.com>
2025-10-25 15:02:44 +0530

    added/modified skills template

---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import { DraggableCardDemo } from "./DragCardPreview";
+
+export default function Skills(){
+    return (
+        <>
+        
+         <div className="min-h-4/5 min-w-4/5 bg-white/10   rounded-2xl" >
+            <DraggableCardDemo />
+         </div>
+        </>
+    )
+}
