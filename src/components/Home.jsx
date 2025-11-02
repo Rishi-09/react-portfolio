e870addc599a1961106a0a4ba1bb8fc324866d50
Rishi <rishikm215@gmail.com>
2025-11-02 21:41:02 +0530

    integrated aceternity + reactbit CLI / background is still issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div className="h-screen overflow-x-hidden  w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
-        <Boxes />
+        <span className="w-fit overflow-x-hidden" > <Boxes /> </span>
         <SpotlightPreview />
       </div>
     </>
