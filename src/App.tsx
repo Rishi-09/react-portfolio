18bdf6cd496361adb2a4dbad91c491d48576c1eb
Rishi <rishikm215@gmail.com>
2025-11-30 21:18:35 +0530

    added smooth scroll , customised skill section

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import Cursor from './ui/Cursor';
+import SmoothScroll from './ui/SmoothScroll';
 
 function App() {
   return (
-    <div className="relative min-h-screen text-slate-200 selection:bg-cyan-500/30">
-      {/* Background Layer */}
-      <LiquidBackground />
-      
-      {/* Navbar */}
-      <Navbar />
-      
-      {/* Main Content */}
-      <main className="relative z-10">
-        <Hero id="home" />
-        <About id="about" />
-        <Projects id="projects" />
-        <Skills id="skills" />
-        <Contact id="contact" />
-      </main>
-    </div>
+    <SmoothScroll>
+      <div className="relative min-h-screen text-slate-200 selection:bg-cyan-500/30">
+        <Cursor />
+        
+        {/* Background Layer */}
+        <LiquidBackground />
+        
+        {/* Navbar */}
+        <Navbar />
+        
+        {/* Main Content */}
+        <main className="relative z-10">
+          <Hero id="home" />
+          <About id="about" />
+          <Projects id="projects" />
+          <Skills id="skills" />
+          <Contact id="contact" />
+        </main>
+      </div>
+    </SmoothScroll>
   );
 }
 
