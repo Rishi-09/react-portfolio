4713a6048b12a74675f45232103439aeeae1565e
Rishi <rishikm215@gmail.com>
2025-12-01 20:37:08 +0530

    false commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Cursor from './ui/Cursor';
-import SmoothScroll from './ui/SmoothScroll';
 import Experience from './components/Experience';
 import Achievements from './components/Achievements';
 import Footer from './components/Footer';
